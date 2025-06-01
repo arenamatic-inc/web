5c329edb5e4700da13bf93b4469204362f13b5e6
Steve MacMullin <stevemacmullin@Steves-MacBook-Air-2.local>
2025-06-01 12:01:05 -0400

    basic working version

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { AuthProvider } from './auth/AuthContext';
 
 const splash = document.getElementById('preload-splash');
 if (splash) splash.remove();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AuthProvider>
     <App />
+    </AuthProvider>
   </StrictMode>,
 )
